64b0f24eb124cbf58a5f6728b374b4eb4c1fecc3
dingshoukun <dingshoukun@quancheng-ec.com>
2017-04-11 15:28:52 +0800

    这是一个hellosdkdemo  好像什么都没有做  只是告诉你如何分发你的包

---
+++ src/main/java/com/quancheng/dsk/Hello.java
@@ -0,0 +1,10 @@
+package com.quancheng.dsk;
+
+/**
+ * Created by dsk on 2017/4/11.
+ */
+public class Hello {
+    public void say(String name){
+        System.out.println("hello"+name);
+    }
+}
